50c9b213d62af7eabd78ada1036e8c4ebbb134bf
Kevin E. Hunt <kevin@kev.in>
2010-12-16 11:48:06 -0800

    Fixes loss of class-level after_response callbacks in cache_classes=true mode

--- lib/after_response/callbacks.rb
+++ lib/after_response/callbacks.rb
@@ -4,13 +4,36 @@ module AfterResponse
     Callbacks.callbacks
   end
 
+  def self.transient_callbacks
+    Callbacks.transient_callbacks
+  end
+
   def self.append_after_response(&block)
     Callbacks.append_after_response(&block)
+  end
+
+  def self.append_transient_after_response(&block)
+    Callbacks.append_transient_after_response(&block)
   end
 
   module Callbacks
 
     module Helpers
+      def self.included(mod)
+        mod.send(:include, TransientHelpers)
+        mod.send(:extend, ModuleHelpers)
+      end
+    end
+
+    # ApplicationController should include this
+    module TransientHelpers
+      def after_response(&block)
+        AfterResponse.append_transient_after_response(&block)
+      end
+    end
+
+    # ApplicationController should extend this
+    module ModuleHelpers
       def after_response(&block)
         AfterResponse.append_after_response(&block)
       end
@@ -24,16 +47,32 @@ module AfterResponse
       end
     end
 
+    def self.append_transient_after_response(&block)
+      if AfterResponse.bufferable?
+        transient_callbacks << block
+      else
+        block.call
+      end
+    end
+
     def self.callbacks
       Thread.current[:__after_response_callbacks__] ||= []
     end
 
+    def self.transient_callbacks
+      Thread.current[:__transient_after_response_callbacks__] ||= []
+    end
+
+    def self.all_callbacks
+      transient_callbacks + callbacks
+    end
+
     def self.perform_after_response_callbacks!
-      AfterResponse.callbacks.each do |b|
+      all_callbacks.each do |b|
         b.call
       end
     ensure
-      AfterResponse.callbacks.clear
+      AfterResponse.transient_callbacks.clear
     end
 
   end
